35ec559999305055fec41c1bfb1609e51be99782
Zachary Brian Spitzer <zspitzer@student00.cse.nd.edu>
2017-12-12 22:46:31 -0500

    Movement shtuff

--- invaderstest.cpp
+++ invaderstest.cpp
@@ -16,16 +16,40 @@ int main(){
 	char c;
 	int x = 250;
 	int y = 250;
+	int x2 = 0;
+	int x3 = 50;
+	int y2 = 100;
+	int y3 = 150;
 	int base = y - 20;
 	int dx = 0;
 	int dy = 0;
+	int dx2 = 1;
 	int ds = 5;
 	int life = 3;
 	gfx_open(500,500,"Final Project");
 	gfx_color(255,255,255);
+
 	while (c != 'q'){
 		gfx_clear();
 		gfx_circle(x + dx, y + dy, 20);
+	        if (x3 + dx2 > 500 || x2 + dx2 < 0){
+        	        dx2 *= -1;
+			y2 += 35;
+			y3 += 35;
+               		gfx_line(x2,y2,x2,y3);
+                	gfx_line(x2,y3,x3,y3);
+               	 	gfx_line(x3,y3,x3,y2);
+               		gfx_line(x3,y2,x2,y2);
+        	}
+        	else{
+                	x2 += dx2;
+                	x3 += dx2;
+                	gfx_line(x2,y2,x2,y3);
+                	gfx_line(x2,y3,x3,y3);
+                	gfx_line(x3,y3,x3,y2);
+                	gfx_line(x3,y2,x2,y2);
+        }
+
 		if (c == ' '){
 			shoot(x, base, ds);
 			base -= ds;
